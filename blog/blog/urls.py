5d66994fe560a3e4c00a9f09c3c7912680d6d15f
MattDwyer14 <mattyd799@gmail.com>
2024-10-20 17:01:46 +0100

    final commit prbably

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('blog_app.urls', namespace='blog_app')),
     path('admin/', admin.site.urls),
     path('blog_app/', include('blog_app.urls', namespace='blog_app'))
 ]
